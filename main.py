6fa4cba2ffb7f68f4d4c3246e99cc8ce5502ab0b
Dejohn-T <Dejohnt.03@gmail.com>
2021-02-18 18:36:16 -0800

    add main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# Create an intro message for the user :) 
+
+print('n------------------Map & Cheese-----------------')
+print('Welcome to Map & Cheese! Been around since 21')
+print('Visit our website -> www.mapandcheese.org')
+print("Our business hours are at the bottom of our page:) ")
+print('-----------------------------------------------------
+
+
+#we want to ask the user for their first and last name.
+first_name = input('Enter your first name:')
+last_name = input("Enter your last name:")
+age = int(input('Enter your age:')
+print(f"Full Name: {first_name} {last_name}")
+correct_name = input('is your name entered correctly?')
+if correct_name != 'yes':
+    first name = input("Enter your first name:")
+    last_name = input('Enter your last name:')
+print(f"updated Full Name: {first_name} {last_name}")
